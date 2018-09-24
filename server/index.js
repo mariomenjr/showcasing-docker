a4b3e44d7673ace72ef33e5f0734e306aae5c056
Mario Menjivar <mariomenjr@gmail.com>
2018-09-23 20:17:35 -0700

    Corrected typo on creation of table postgres

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ const pgClient = new Pool({
 pgClient.on("error", () => console.log("Lost PG connection"));
 
 pgClient
-  .query("CREATE TABLE IF NOT EXIST values(number INT)")
+  .query("CREATE TABLE IF NOT EXISTS values (number INT)")
   .catch(err => console.log(err));
 
 // Redis Client Setup
