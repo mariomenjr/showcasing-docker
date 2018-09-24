a69aedd736ee8b4487a36b46134bcf7309197cbf
Mario Menjivar <mariomenjr@gmail.com>
2018-09-23 18:46:04 -0700

    First commit

---
+++ client/src/OthePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default () => {
+  return (
+    <div>
+      Im in the other page!
+      <link to="/">Go back home!</link>
+    </div>
+  );
+};
